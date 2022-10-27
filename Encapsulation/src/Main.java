2e6cd2c450e60410a6bc69368735184bd1214cb3
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2022-10-28 03:20:12 +0530

    Add files via upload

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       EnhancedPlayer ep= new EnhancedPlayer("Tim",50,"Sword");
+        System.out.println("Initial health is"+ ep.getHealth());
+
+
+
+    }
+}
